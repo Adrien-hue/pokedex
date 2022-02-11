ca7991f7d78493a2e291925dc83c7235a313fd67
Adrien_hue <adrien.houee1@gmail.com>
2022-02-11 14:32:32 +0100

    Design : set up basic design

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width="150px" height="150px"/>
+        <h1>Pokédex</h1>
       </header>
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element="" />
         </Routes>
       </main>
     </div>
   );
-}
-
-function Home() {
-  return <h1>Pokédex</h1>
 }
 
 export default App;
